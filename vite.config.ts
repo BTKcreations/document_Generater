537b9cd57bafe128e9468eee1921073fa72b84b7
TharunKumar <buddetharunkumar123@gmail.com>
2024-11-05 21:45:17 +0530

    gh

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/document_Generater/', // Ensure this matches your GitHub repo name
   plugins: [react()],
+  build: {
+    outDir: 'dist' // Ensure this is set to 'dist'
+  }
 });
